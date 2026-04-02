da212df67d559361fa956add2ab1f7d6d14bd1d9
Carlos Ayala <am25008@ues.edu.sv>
2026-04-02 17:53:29 -0600

    feat: clase main del sistema

---
+++ src/com/carlos/inventary/Main.java
@@ -0,0 +1,12 @@
+package com.carlos.inventary;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InventarioFrontend app = new InventarioFrontend();
+		
+		app.run();
+	}
+
+}
